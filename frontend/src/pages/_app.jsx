9323c0664f43dea0dc607bddeafa42cce5cc6cd2
Aracelli Rivas <aracelli.rivas2101@alumnos.ubiobio.cl>
2024-10-27 20:21:32 -0300

    Refactor font styles and add Montserrat as the default font family

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,13 +1,15 @@
 // pages/_app.js
 import { Montserrat } from 'next/font/google';
 import Layout from '@components/Layout';
-import '@styles/globals.css'; // Asegúrate de que la ruta es correcta
+import '@styles/globals.css'; 
 import { AuthProvider } from '../context/authContext';
 import { useRouter } from 'next/router'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['400', '700'], // Puedes ajustar los pesos según tus necesidades
+  weight: ['400', '700'],
+  display: 'swap', 
+  variable: '--font-sans',
 });
 
 export default function MyApp({ Component, pageProps }) {
@@ -15,7 +17,12 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <AuthProvider>
-      <Layout key={router.asPath}>
+      <Layout key={router.asPath}className={montserrat.variable}>
+        <style jsx global>{`
+          html {
+            font-family: var(--font-sans);
+          }
+        `}</style>
         <Component {...pageProps} />
       </Layout>
     </AuthProvider>
