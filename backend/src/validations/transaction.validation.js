459aee78fad6015b50b9334d8583356d29bae4a8
Anaís Scarlett <103465473+anaisxsly@users.noreply.github.com>
2024-11-11 01:27:54 -0300

    Update transaction.validation.js

--- backend/src/validations/transaction.validation.js
+++ backend/src/validations/transaction.validation.js
@@ -2,20 +2,20 @@
 const Joi = require('joi');
 
 const createTransactionSchema = Joi.object({
-  tipo: Joi.string().valid('ingreso', 'egreso').required().messages({
+  transaction_type: Joi.string().valid('ingreso', 'egreso').required().messages({
     'any.required': 'El tipo de transacción es obligatorio',
     'any.only': 'El tipo de transacción debe ser ingreso o egreso'
   }),
-  monto: Joi.number().positive().required().messages({
+  amount: Joi.number().positive().required().messages({
     'number.base': 'El monto debe ser un número',
     'number.positive': 'El monto debe ser un número positivo',
     'any.required': 'El monto es obligatorio'
   }),
-  metodo_pago: Joi.string().valid('efectivo', 'transferencia').required().messages({
+  payment_method: Joi.string().valid('efectivo', 'transferencia').required().messages({
     'any.required': 'El método de pago es obligatorio',
     'any.only': 'El método de pago debe ser efectivo o transferencia'
   }),
-  descripcion: Joi.string().min(3).max(100).required().messages({
+  description: Joi.string().min(3).max(100).required().messages({
     'string.min': 'La descripción debe tener al menos 3 caracteres',
     'string.max': 'La descripción no puede tener más de 100 caracteres',
     'any.required': 'La descripción es obligatoria'
